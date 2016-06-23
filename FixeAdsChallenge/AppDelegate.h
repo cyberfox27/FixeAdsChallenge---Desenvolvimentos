ebe88e77b4929c42366ee361d90a780e0cff0307
cyberfox27 <pedromrvalerio@gmail.com>
2016-06-23 10:03:14 +0100

    Initial Commit

---
+++ FixeAdsChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FixeAdsChallenge
+//
+//  Created by Warzone on 23/06/16.
+//  Copyright © 2016 fixeads. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FixeAdsChallenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FixeAdsChallenge
+//
+//  Created by Warzone on 23/06/16.
+//  Copyright © 2016 fixeads. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
